7fcc85de988329017164321b45e98f7ef5ca2542
userD02 <panthadiwas@gmail.com>
2024-03-09 07:36:59 +0545

    MVP basic

--- app/main.cpp
+++ app/main.cpp
@@ -112,10 +112,13 @@ int main()
     IndexBuffer ib(indices, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0, -100, 0));
+    glm::mat4 mvp = proj * view * model;
 
     Shader shader("res/shader/texture.shader");
     shader.Bind();
-    shader.SetUniformMat4f("u_MVP", proj);
+    shader.SetUniformMat4f("u_MVP", mvp);
     Renderer render;
     Texture texture("res/textures/tbate.png");
     texture.Bind();
